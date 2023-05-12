744648eca58a64f840cd1774395e86ec1a4e7924
human <human@DESKTOP-SLFRG6Q>
2023-05-12 18:11:07 +0900

    Second commit - Setting

---
+++ Final_SOUPIE_JobBoard/src/main/java/com/human/java/domain/JobAdVO.java
@@ -0,0 +1,22 @@
+package com.human.java.domain;
+
+import lombok.Data;
+
+@Data
+public class JobAdVO {
+	
+	private int JOB_AD_ID;
+	private String USERID;
+	private String JOB_COMPANY_LOGO;
+	private String JOB_COMPANY_ADDR;
+	private String JOB_AD_TITLE;
+	private String JOB_AD_CAREER;
+	private String JOB_AD_EDUCATION;
+	private String JOB_AD_HIRE_TYPE;
+	private String JOB_AD_SALARY;
+	private String JOB_AD_PART;
+	private String JOB_BENEFIT;
+	private String CREATED_AT;
+	
+
+}
